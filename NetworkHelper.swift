11b10fb82707df860295c589cb4f308c1e80ad33
Alex Paul <alexpaul@users.noreply.github.com>
2018-12-14 05:09:10 -0500

    Update NetworkHelper.swift

--- NetworkHelper.swift
+++ NetworkHelper.swift
@@ -1,7 +1,31 @@
 #import Foundation 
 
-final class NetworkHeloper {
-  static func performDataTask(completionHandler: @escaping (APIError?, Data?) -> Void) {
-    
+final class NetworkHelper {
+  static func performDataTask(urlString: String, httpMethod: String, completionHandler: @escaping (APIError?, Data?) ->Void) {
+    guard let url = URL(string: urlString) else {
+      completionHandler(APIError.badURL("badURL: \(urlString)"), nil)
+      return
+    }
+    var request = URLRequest(url: url)
+    request.httpMethod = httpMethod
+    URLSession.shared.dataTask(with: request) { (data, response, error) in
+      if let error = error {
+        completionHandler(APIError.networkError(error), nil)
+      }
+      if let response = response as? HTTPURLResponse {
+        print("response status code is \(response.statusCode)")
+        guard response.statusCode >= 200 && response.statusCode < 300 else {
+          if let data = data {
+            completionHandler(APIError.badStatusCode(response.statusCode), data)
+          } else {
+            completionHandler(APIError.badStatusCode(response.statusCode), nil)
+          }
+          return
+        }
+      }
+      if let data = data {
+        completionHandler(nil, data)
+      }
+    }.resume()
   }
 }
