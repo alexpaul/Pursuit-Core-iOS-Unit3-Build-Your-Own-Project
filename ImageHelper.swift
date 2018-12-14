2e81e27872055c4ee9f11fc2d7d6b9c48d69e2be
Alex Paul <alexpaul@users.noreply.github.com>
2018-12-14 05:08:42 -0500

    Update ImageHelper.swift

--- ImageHelper.swift
+++ ImageHelper.swift
@@ -1,5 +1,30 @@
 final class ImageHelper {
-  static func fetchImage(from url: URL, completionHandler: (APIError?, UIImage?) -> Void) {
+  static let shared = ImageHelper()
+  private let cache: NSCache<NSString, UIImage>?
+  private let sharedCache = NSCache<NSString, UIImage>()
   
+  private init() {
+    cache = NSCache()
+  }
+  
+  static func fetchImage(with urlString: String, completionHandler: @escaping (APIError?, UIImage?) -> Void) {
+    NetworkHelper.performDataTask(urlString: urlString, httpMethod: "GET") { (error, data) in
+      if let error = error {
+        completionHandler(APIError.networkError(error), nil)
+      } else if let data = data {
+          let image = UIImage(data: data)
+          DispatchQueue.main.async {
+            completionHandler(nil, image)
+          }
+      }
+    }
+  }
+  
+  public func cachedImage(url: String) -> UIImage? {
+    return sharedCache.object(forKey: url as NSString)
+  }
+  
+  public func setImage(key: String, image: UIImage) {
+    sharedCache.setObject(image, forKey: key as NSString)
   }
 }
