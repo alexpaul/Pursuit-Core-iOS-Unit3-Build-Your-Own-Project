672aef1af79a90b25248d7805d5d32c1fea09b11
Alex Paul <alexpaul@users.noreply.github.com>
2018-12-14 05:08:15 -0500

    Update APIError.swift

--- APIError.swift
+++ APIError.swift
@@ -1,5 +1,19 @@
-enum APIError: Error {
-  case badURL 
-  case networkError
-  case decodingError
+public enum APIError: Error {
+  case badURL(String)
+  case decodingError(Error)
+  case networkError(Error)
+  case badStatusCode(Int)
+  
+  public func getErrorMessage() -> String {
+    switch self {
+    case .badURL(let badURL):
+      return badURL
+    case .decodingError(let error):
+      return "decoding error: \(error)"
+    case .networkError(let error):
+      return "network error: \(error)"
+    case .badStatusCode(let statusCode):
+      return "status code is \(statusCode)"
+    }
+  }
 }
