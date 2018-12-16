79565bb3b1a413b97c85919704edbe7af1cead9d
Alex Paul <alexpaul@users.noreply.github.com>
2018-12-16 07:53:16 -0500

    Update NetworkHelper.swift

--- NetworkHelper.swift
+++ NetworkHelper.swift
@@ -1,31 +1,26 @@
 #import Foundation 
+import Foundation
 
 final class NetworkHelper {
-  static func performDataTask(urlString: String, httpMethod: String, completionHandler: @escaping (APIError?, Data?) ->Void) {
+  static func performDataTask(urlString: String,
+                              httpMethod: String,
+                              completionHandler: @escaping (AppError?, Data?, HTTPURLResponse?) ->Void) {
     guard let url = URL(string: urlString) else {
-      completionHandler(APIError.badURL("badURL: \(urlString)"), nil)
+      completionHandler(AppError.badURL("\(urlString)"), nil, nil)
       return
     }
     var request = URLRequest(url: url)
     request.httpMethod = httpMethod
     URLSession.shared.dataTask(with: request) { (data, response, error) in
+      guard let response = response as? HTTPURLResponse else {
+        completionHandler(AppError.noResponse, nil, nil)
+        return
+      }
+      print("response status code is \(response.statusCode)")
       if let error = error {
-        completionHandler(APIError.networkError(error), nil)
-      }
-      if let response = response as? HTTPURLResponse {
-        print("response status code is \(response.statusCode)")
-        guard response.statusCode >= 200 && response.statusCode < 300 else {
-          if let data = data {
-            completionHandler(APIError.badStatusCode(response.statusCode), data)
-          } else {
-            completionHandler(APIError.badStatusCode(response.statusCode), nil)
-          }
-          return
-        }
-        completionHandler(nil, nil)
-      }
-      if let data = data {
-        completionHandler(nil, data)
+        completionHandler(AppError.networkError(error), nil, response)
+      } else if let data = data {
+        completionHandler(nil, data, response)
       }
     }.resume()
   }
