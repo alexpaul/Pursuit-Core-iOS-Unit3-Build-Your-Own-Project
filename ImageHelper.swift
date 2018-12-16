49eb158894ee0b3a2a5eb17859ca2e6371b73fd8
Alex Paul <alexpaul@users.noreply.github.com>
2018-12-16 07:51:38 -0500

    Update ImageHelper.swift

--- ImageHelper.swift
+++ ImageHelper.swift
@@ -1,30 +1,22 @@
+import UIKit
+
 final class ImageHelper {
-  static let shared = ImageHelper()
-  private let cache: NSCache<NSString, UIImage>?
-  private let sharedCache = NSCache<NSString, UIImage>()
-  
-  private init() {
-    cache = NSCache()
-  }
-  
-  static func fetchImage(with urlString: String, completionHandler: @escaping (APIError?, UIImage?) -> Void) {
-    NetworkHelper.performDataTask(urlString: urlString, httpMethod: "GET") { (error, data) in
+  static func fetchImage(urlString: String, completionHandler: @escaping (AppError?, UIImage?) -> Void) {
+    NetworkHelper.performDataTask(urlString: urlString, httpMethod: "GET") { (error, data, response) in
       if let error = error {
-        completionHandler(APIError.networkError(error), nil)
+        completionHandler(error, nil)
       } else if let data = data {
-          let image = UIImage(data: data)
-          DispatchQueue.main.async {
-            completionHandler(nil, image)
-          }
+        let image = UIImage(data: data)
+        DispatchQueue.main.async {
+          completionHandler(nil, image)
+        }
       }
     }
-  }
-  
-  public func cachedImage(url: String) -> UIImage? {
-    return sharedCache.object(forKey: url as NSString)
-  }
-  
-  public func setImage(key: String, image: UIImage) {
-    sharedCache.setObject(image, forKey: key as NSString)
+    // NSCache - a dictionary that has the ability to cache transient data
+    // for performance improvements
+    // Use a Singleton to build a wrapper around NSCache
   }
 }
+// use
+// im in a view controller
+// ImageHelper.fetchImage(urlString: "https:......") { error, image in }
